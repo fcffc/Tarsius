fe21ed0784eada743182a0ecab6e003437d5c2bb
Fernanda Ferreira <Fernanda Ferreira@CTJ-23A-175548>
2016-11-08 19:00:51 -0300

    Commit - Classes CargoDAO e ChecklistDAO estendendo a classe GenericDAO.

---
+++ src/main/java/br/jus/tjmt/tarsius/dao/CargoDAO.java
@@ -0,0 +1,7 @@
+package br.jus.tjmt.tarsius.dao;
+
+import br.jus.tjmt.tarsius.domain.Cargo;
+
+public class CargoDAO extends GenericDAO<Cargo> {
+
+}
---
+++ src/main/java/br/jus/tjmt/tarsius/dao/ChecklistDAO.java
@@ -0,0 +1,7 @@
+package br.jus.tjmt.tarsius.dao;
+
+import br.jus.tjmt.tarsius.domain.Checklist;
+
+public class ChecklistDAO extends GenericDAO<Checklist> {
+
+}
