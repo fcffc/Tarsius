69707ec5fa9c0cdbaca9d599bfa2e5648f2e37f5
fcffc <fcffc@hotmail.com>
2016-11-10 01:38:50 -0300

    Commit - Criado o metodo Excluir da classe CargoBean.

--- src/main/java/br/jus/tjmt/tarsius/bean/CargoBean.java
+++ src/main/java/br/jus/tjmt/tarsius/bean/CargoBean.java
@@ -6,6 +6,7 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.faces.event.ActionEvent;
 
 import org.omnifaces.util.Messages;
 
@@ -47,7 +48,7 @@ public class CargoBean implements Serializable {
 		}
 	}
 
-	// Instancia a classe para novo registro
+	// Inicia uma nova instancia ao abrir a tela de cadastro/edição
 	public void novo() {
 		cargo = new Cargo();
 	}
@@ -55,11 +56,40 @@ public class CargoBean implements Serializable {
 	public void salvar() {
 		try {
 			CargoDAO cargoDAO = new CargoDAO();
-			cargoDAO.salvar(cargo);
+			cargoDAO.merge(cargo);
+			// Reinstancia o Cargo
 			novo(); // Limpa a tela após inserção dos dados
+			// Recarrega a listagem de cargo
+			cargos = cargoDAO.listar();
 			Messages.addGlobalInfo("Cargo salvo com sucesso.");
 		} catch (RuntimeException erro) {
 			Messages.addGlobalError("Falha ao tentar salvar o Cargo.");
+			erro.printStackTrace();
+		}
+	}
+
+	public void excluir(ActionEvent evento) {
+		try {
+			// Seleciona o cargo a ser excluído
+			cargo = (Cargo) evento.getComponent().getAttributes().get("cargoSelecionado");
+			// Exclui o cargo
+			CargoDAO cargoDAO = new CargoDAO();
+			cargoDAO.excluir(cargo);
+			// Recarrega a listagem de cargo
+			cargos = cargoDAO.listar("nome");
+			Messages.addGlobalInfo("Cargo : " + cargo.getNome() + " excluído com sucesso.");
+		} catch (RuntimeException erro) {
+			Messages.addFlashGlobalError("Falha ao excluir Cargo.");
+			erro.printStackTrace();
+		}
+	}
+
+	public void editar(ActionEvent evento) {
+		try {
+			// Seleciona o estado a ser editado
+			cargo = (Cargo) evento.getComponent().getAttributes().get("cargoSelecionado");
+		} catch (RuntimeException erro) {
+			Messages.addFlashGlobalError("Falha ao carregar tela de edição.");
 			erro.printStackTrace();
 		}
 	}
